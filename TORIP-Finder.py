f5c15614df8cf33da6f4e4761aa276dccea61ceb
sarahmoshfeghi <134586796+sarahmoshfeghi@users.noreply.github.com>
2025-03-10 17:06:09 +0330

    Update TORIP-Finder.py

--- TORIP-Finder.py
+++ TORIP-Finder.py
@@ -1,13 +1,15 @@
 import requests
 import re
 import pymongo
+from mail_send import send_mail
 
 
 def ip_block():
-    resource = ['https://feodotracker.abuse.ch/downloads/ipblocklist_recommended.txt', 'https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt'
-                ,'https://www.dan.me.uk/torlist/?full']
+    resource = ['https://feodotracker.abuse.ch/downloads/ipblocklist_recommended.txt',
+                'https://rules.emergingthreats.net/fwrules/emerging-Block-IPs.txt'
+        , 'https://www.dan.me.uk/torlist/?full']
     ip_block_list = []
-    file = open('ip-block.csv', 'w')
+    file = open('/pathtothefile/ip-block.csv', 'w')
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     mydb = myclient['IPBlockRecommended']
     mycol = mydb['IPBlockRecommended']
@@ -17,16 +19,22 @@ def ip_block():
         result = re.findall(r"^\d.*", r.text, re.MULTILINE)
         if result:
             for ip in result:
+                if ':' in ip:
+                    continue
+                # print(ip)
                 ip_j = {}
                 ip_j['source'] = source
                 ip = ip.strip()
                 if mycol.count_documents({'ip': ip}, limit=1) != 0:
                     continue
                 ip_j['ip'] = ip
-                if ':' in ip:
-                    continue
                 mycol.insert_one(ip_j)
                 print(ip)
-                file.write(ip+'\n')
+                file.write(ip + '\n')
+    file.close()
+
+    send_mail("test@test.com", "test2@test.com",
+              "TOR-IP-Block")
+
 
 ip_block()
